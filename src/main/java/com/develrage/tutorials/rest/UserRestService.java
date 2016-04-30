c4570b96ce40c281b9a29b97f7e626bf36de8b42
melangoth <contact@develrage.com>
2016-04-30 15:55:10 +0200

    added getUserByName

--- src/main/java/com/develrage/tutorials/rest/UserRestService.java
+++ src/main/java/com/develrage/tutorials/rest/UserRestService.java
@@ -2,6 +2,7 @@ package com.develrage.tutorials.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
@@ -12,6 +13,17 @@ public class UserRestService {
         StringBuilder output = new StringBuilder();
 
         output.append("getUser is called");
+        Helper.appendAppVersion(output);
+
+        return Response.status(200).entity(output.toString()).build();
+    }
+
+    @GET
+    @Path("{name}")
+    public Response getUserByName(@PathParam("name") String name) {
+        StringBuilder output = new StringBuilder();
+
+        output.append("getUserByName is called");
         Helper.appendAppVersion(output);
 
         return Response.status(200).entity(output.toString()).build();

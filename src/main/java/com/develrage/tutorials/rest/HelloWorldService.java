4c5c1fd28aaa687e39cd236d6cf60667e0264b76
melangoth <contact@develrage.com>
2016-04-30 14:06:24 +0200

    added app version display support, pom preconfigured for next tutorial

--- src/main/java/com/develrage/tutorials/rest/HelloWorldService.java
+++ src/main/java/com/develrage/tutorials/rest/HelloWorldService.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Properties;
 
 @Path("/hello")
 public class HelloWorldService {
@@ -11,8 +13,28 @@ public class HelloWorldService {
     @GET
     @Path("/{param}")
     public Response getMsg(@PathParam("param") String msg) {
-        String output = String.format("Jersey say: %s! So nice!", msg);
+        String output = String.format("RESTEasy say: %s! So nice! App version: %s", msg, getAppVersion());
 
         return Response.status(200).entity(output).build();
+    }
+
+    private String getAppVersion() {
+        String appversion = "app.version.not-found";
+
+        final Properties props = new Properties();
+        try {
+            props.load(HelloWorldService.class.getClassLoader().getResourceAsStream("appversion.properties"));
+            if (props.containsKey("appversion")) {
+                appversion = props.getProperty("appversion");
+            }
+        } catch (IOException ioe) {
+            appversion = "IOException";
+        } catch (NullPointerException npe) {
+            appversion = "NullPointerException";
+        } catch (Exception e) {
+            appversion = "Exception";
+        }
+
+        return appversion;
     }
 }
